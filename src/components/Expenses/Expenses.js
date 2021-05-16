52416c04c1a452a437d75809a95cc8066cbdc60f
Sahibpreet Kaur <sahibkaur81@gmail.com>
2021-05-16 23:16:38 +0530

    adds code to add text when no expense is found

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,18 +13,22 @@ function Expenses(props) {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
+    let expensesContent = <p>No expenses found</p>;
+    if (filteredExpenses.length>0) {
+        filteredExpenses.map((expense) => (
+            <ExpenseItem 
+            key={expense.id}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} 
+            />
+        ))
+    }
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>        
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date} 
-                    />
-                ))}        
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {expensesContent}   
             </Card>
         </div>
         
